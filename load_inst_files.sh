ffb5b4ca3f4c52da38dfa3fe7ad66128414f2a63
Flavio Del Mestre <flavio@perunis.ch>
2023-06-09 16:10:54 +0200

    Update load_inst_files.sh

--- load_inst_files.sh
+++ load_inst_files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/fdmgit/install-debian-11/main/bashrc.ini
-wget https://raw.githubusercontent.com/fdmgit/install-debian-11/main/instubp1.sh
-wget https://raw.githubusercontent.com/fdmgit/install-debian-11/main/instubp2.sh
+wget https://raw.githubusercontent.com/fdmgit/install-debian-11/main/instp1.sh
+wget https://raw.githubusercontent.com/fdmgit/install-debian-11/main/instp2.sh
 
 cp bashrc.ini /root/.bashrc
 rm /root/bashrc.ini
 
-chmod +x /root/instubp1.sh
-chmod +x /root/instubp2.sh
+chmod +x /root/instp1.sh
+chmod +x /root/instp2.sh
