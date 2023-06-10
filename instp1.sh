3f8f534dd0f4e359ebc55f1c4a006c2be7a2fca3
Flavio Del Mestre <flavio@perunis.ch>
2023-06-10 15:06:56 +0200

    Update instp1.sh

--- instp1.sh
+++ instp1.sh
@@ -20,6 +20,20 @@ apt update
 #passwd root
 echo "root:$1" | chpasswd   # set root password -
 
+###################################
+#### Add gat (replacement for cat)
+###################################
+
+cd /usr/local/bin
+wget https://github.com/koki-develop/gat/releases/download/v0.8.2/gat_Linux_x86_64.tar.gz
+tar -xvzf gat_Linux_arm64.tar.gz
+chown root:root gat
+chmod +x gat
+rm gat_Linux_arm64.tar.gz
+rm LICENSE
+rm README.md
+
+
 
 ##############################
 #### Install Virtualmin
@@ -29,4 +43,5 @@ echo "root:$1" | chpasswd   # set root password -
 wget -O virtualmin-install.sh https://raw.githubusercontent.com/virtualmin/virtualmin-install/master/virtualmin-install.sh
 #sh virtualmin-install-pre-release.sh -y
 sh virtualmin-install.sh -y
+rm virtualmin-install.sh
 reboot
