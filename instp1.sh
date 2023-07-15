7de65f88a6da4af91ab410514cceac241c40f3df
Flavio Del Mestre <flavio@perunis.ch>
2023-07-15 13:22:46 +0200

    Update instp1.sh

--- instp1.sh
+++ instp1.sh
@@ -7,7 +7,8 @@ apt update
 ###################################
 
 apt upgrade -y 
-apt install plocate sntp ntpdate software-properties-common -y 
+apt install plocate sntp ntpdate software-properties-common -y
+timedatectl set-timezone Europe/Zurich
 hostnamectl set-hostname $2
 
 ##############################
@@ -33,6 +34,27 @@ rm gat_Linux_x86_64.tar.gz
 rm LICENSE
 rm README.md
 
+###################################
+#### Add joshuto (cli filemanager)
+###################################
+
+cd /usr/local/bin
+wget https://github.com/kamiyaa/joshuto/releases/download/v0.9.4/joshuto-v0.9.4-x86_64-unknown-linux-gnu.tar.gz
+tar -xvzf joshuto-v0.9.4-x86_64-unknown-linux-gnu.tar.gz
+tar -vxzf joshuto-v0.9.4-x86_64-unknown-linux-gnu.tar.gz -C /usr/local/bin  --strip-components=1
+chown root:root joshuto
+chmod +x joshuto
+rm joshuto-v0.9.4-x86_64-unknown-linux-gnu.tar.gz
+
+
+###################################
+#### Build aliases file
+###################################
+
+cd /root
+touch .bash_aliases
+echo "alias jos='joshuto'" >> .bash_aliases
+echo "alias gc='gat'" >> .bash_aliases
 
 
 ##############################
